9ddaa172e238f77ee19f3658eac86782549d87ba
Matt @ Home <matthome@Scobels-Macbook-Air.local>
2015-11-22 21:58:52 -0500

    added first files for Red Blue

--- RedBlue/ViewController.swift
+++ RedBlue/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hideRedIcon: UIButton!
+    @IBOutlet weak var hideBlueIcon: UIButton!
+    @IBOutlet weak var blueIcon: UIImageView!
+    @IBOutlet weak var redIcon: UIImageView!
+    @IBOutlet weak var showRedIcon: UIButton!
+    @IBOutlet weak var showBlueIcon: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +25,30 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func hideRedIcon(sender: AnyObject) {
+        redIcon.hidden = true
+        showRedIcon.hidden = false
+        hideRedIcon.hidden = true
+    }
+    @IBAction func hideBlueIcon(sender: AnyObject) {
+        blueIcon.hidden = true
+        showBlueIcon.hidden = false
+        hideBlueIcon.hidden = true
+    }
+    
+    @IBAction func showRedIcon(sender: AnyObject) {
+        redIcon.hidden = false
+        showRedIcon.hidden = true
+        hideRedIcon.hidden = false
+    }
+    
+    
+    @IBAction func showBlueIcon(sender: AnyObject) {
+        blueIcon.hidden = false
+        showBlueIcon.hidden = true
+        hideBlueIcon.hidden = false
     }
 
 
